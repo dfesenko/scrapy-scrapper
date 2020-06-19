8904604b83b990e826d7906323787fa390329a75
dfesenko5 <fesenkogithub@gmail.com>
2020-06-19 22:19:01 +0300

    Refactoring: transfer staticmethods inside the parse_profile() method.
    As a result, this should increase the code readability and understanding of the script structure.

--- morganlewis/morganlewis/spiders/morganlewis_scraper.py
+++ morganlewis/morganlewis/spiders/morganlewis_scraper.py
@@ -7,27 +7,11 @@ from datetime import datetime
 class MorganlewisSpider(scrapy.Spider):
     name = 'morganlewis'
 
-    @staticmethod
-    def generate_paging_url(page_num, num_per_page=100):
+    def generate_paging_url(self, page_num, num_per_page=100):
         return f"https://www.morganlewis.com/api/custom/peoplesearch/search?keyword=&category" \
                f"=bb82d24a9d7a45bd938533994c4e775a&sortBy=lastname&pageNum={page_num}&numberPerPage={num_per_page}" \
                f"&numberPerSection=5&enforceLanguage=&languageToEnforce=&school=&position=&location=&court=&judge" \
                f"=&isFacetRefresh=false "
-
-    @staticmethod
-    def get_absolute_photo_url(response):
-        relative_photo_url = response.css('div.thumbnail img ::attr(src)').get()
-        return response.urljoin(relative_photo_url)
-
-    @staticmethod
-    def get_services(response):
-        # detect in which section Services are - in case Services and Regions sections will be swapped
-        first_block_person_depart_info_head = response.css('section.person-depart-info:first-of-type h2 ::text').get()
-
-        if first_block_person_depart_info_head == 'Services':
-            return response.css('section.person-depart-info:first-of-type a ::text').getall()
-        else:
-            return response.css('section.person-depart-info:nth-of-type(2) a ::text').getall()
 
     def start_requests(self):
         yield scrapy.Request(url=self.generate_paging_url(page_num=1), callback=self.parse)
@@ -44,35 +28,49 @@ class MorganlewisSpider(scrapy.Spider):
             yield response.follow(self.generate_paging_url(page_num=current_page_num + 1), callback=self.parse)
 
     def parse_profile(self, response):
+
+        def get_absolute_photo_url(response):
+            relative_photo_url = response.css('div.thumbnail img ::attr(src)').get()
+            return response.urljoin(relative_photo_url)
+
+        def get_services(response):
+            # detect in which section Services are - in case Services and Regions sections will be swapped
+            first_block_person_depart_info_head = response.css(
+                'section.person-depart-info:first-of-type h2 ::text').get()
+            if first_block_person_depart_info_head == 'Services':
+                return response.css('section.person-depart-info:first-of-type a ::text').getall()
+            else:
+                return response.css('section.person-depart-info:nth-of-type(2) a ::text').getall()
+
+        def get_person_id(response):
+            vcard_url = response.css('div.thumbnail-details p.v-card a::attr(href)').get()
+            return vcard_url.split('itemId=%7B')[-1][:-3]
+
+        def get_publications(response):
+            profile_data = response.meta['scrapped_data']
+            profile_data['publications'] = response.css('a::attr(title)').getall()
+            yield profile_data
+
         profile_data = {
             'url': response.request.url,
-            'photo_url': self.get_absolute_photo_url(response),
+            'photo_url': get_absolute_photo_url(response),
             'full_name': response.css('section.person-heading span[itemprop=name] ::text').get(),
             'position': response.css('section.person-heading h2 ::text').get(),
             'phone_numbers': response.css('div.thumbnail-details p[itemprop=telephone] a ::text').getall(),
             'email': response.css('div.thumbnail-details a[itemprop=email] ::text').get(),
-            'services': self.get_services(response),
+            'services': get_services(response),
             'sectors': response.css('div.person-depart-info a ::text').getall(),
             'person_brief': response.css('div.people-intro p ::text').get(),
             'scrapping_datetime': datetime.now().strftime("%Y:%m:%d, %H:%M:%S")
         }
 
         yield scrapy.Request(url="https://www.morganlewis.com/api/sitecore/accordion/getaccordionlist",
-                             callback=self.get_publications,
+                             callback=get_publications,
                              method='POST',
                              meta={'scrapped_data': profile_data},
-                             body=json.dumps({'itemId': '{' + self.get_person_id(response) + '}',
+                             body=json.dumps({'itemId': '{' + get_person_id(response) + '}',
                                               'itemType': 'publicationitemlist',
                                               'printView': ''}),
                              headers={'Content-Type': 'application/json'})
 
-    @staticmethod
-    def get_person_id(response):
-        vcard_url = response.css('div.thumbnail-details p.v-card a::attr(href)').get()
-        return vcard_url.split('itemId=%7B')[-1][:-3]
 
-    @staticmethod
-    def get_publications(response):
-        profile_data = response.meta['scrapped_data']
-        profile_data['publications'] = response.css('a::attr(title)').getall()
-        yield profile_data
