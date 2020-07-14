cfc14e9ca206d1b86fb672bcb730b957be5829c1
dfesenko5 <fesenkogithub@gmail.com>
2020-07-14 21:42:21 +0300

    Implemented pipeline for publishing scrapped items to RabbitMQ queue

--- morganlewis/morganlewis/pipelines.py
+++ morganlewis/morganlewis/pipelines.py
@@ -5,7 +5,36 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import pika
+
 
 class MorganlewisPipeline:
+
+    def __init__(self, rabbitmq_host, rabbitmq_queue_name):
+        self.rabbitmq_host = rabbitmq_host
+        self.rabbitmq_queue_name = rabbitmq_queue_name
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            rabbitmq_host=crawler.settings.get('RABBITMQ_HOST'),
+            rabbitmq_queue_name=crawler.settings.get('RABBITMQ_QUEUE_NAME', 'morganlewis_people')
+        )
+
+    def open_spider(self, spider):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.rabbitmq_host))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=self.rabbitmq_queue_name, durable=True)
+
+    def close_spider(self, spider):
+        self.connection.close()
+
     def process_item(self, item, spider):
+        self.channel.basic_publish(
+            exchange='',
+            routing_key=self.rabbitmq_queue_name,
+            body=str(item),
+            properties=pika.BasicProperties(
+                delivery_mode=2,
+            ))
         return item
--- morganlewis/morganlewis/settings.py
+++ morganlewis/morganlewis/settings.py
@@ -64,9 +64,9 @@ COOKIES_ENABLED = False
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'morganlewis.pipelines.MorganlewisPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'morganlewis.pipelines.MorganlewisPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
@@ -88,3 +88,6 @@ COOKIES_ENABLED = False
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+RABBITMQ_HOST = 'localhost'
+RABBITMQ_QUEUE_NAME = 'test_queue'
